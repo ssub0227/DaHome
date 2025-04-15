5b2e2617597bbcdc20c8d0f0b4f2113c0f1a564f
ssub0227 <sb010227@gmail.com>
2025-04-15 21:18:07 +0200

    Add: Upload 페이지 css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './Home.css'
-import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 
 const Home = () => {
   return (
     <div className='home'> 
       <div className='intro one'>
-        <h3 className='intro_title'>
+        <h3 className='title'>
           다 같이 홈트레이닝 프로젝트
-          <p className='intro_title_impact'>다홈</p>
+          <p className='title_impact'>다홈</p>
         </h3>
         <p className='intro_sub_title'>원하는 홈트레이닝 영상을 등록하고<br/>친구화 함께 운동해요.</p>
         <Button link={'/upload'} text={'시작하기'}></Button>
       </div>
       <div className='intro two'>
-        <h3 className='intro_title'>운동은 해야 하는데, <br/>밖에 나가기 싫어!</h3>
+        <h3 className='title'>운동은 해야 하는데, <br/>밖에 나가기 싫어!</h3>
         <p className='intro_sub_title'>집에서도 쉽게<br/>유튜브 영상으로 운동해요.</p>
       </div>
       <div className='intro three'>
-        <h3 className='intro_title'>혼자서는 못하겠는 홈트레이닝</h3>
-        <p className='intro_sub_title'>친구들과 함께라면<br/>무엇이든 할 수 있어요.</p>
+        <h3 className='title'>혼자서는 못하겠는 홈트레이닝</h3>
+        <p className='sub_title'>친구들과 함께라면<br/>무엇이든 할 수 있어요.</p>
         <Button link={'/upload'} text={'영상 등록하기'}></Button>
       </div>
     </div>
--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Upload.css'
+import Button from '../../components/Button/Button';
 
 const Upload = () => {
   return (
     <div className='upload'>
-      <input type='text' placeholder='운동방 이름을 입력하세요.'></input>
-      <input type='text' placeholder='유튜르 링크를 입력하세요.'></input>
-      <Link to='/Share'><button type='submit'></button></Link>
-      <div>
-        <h2>미리보기</h2>
-        <div>
-          <div>
-            <iframe src="https://www.youtube.com/embed/6ZUIwj3FgUY?si=xnoj-K7I8WLBqhKF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h2 className='title'>다홈 프로젝트 생성</h2>
+      <input type='text' placeholder='프로젝트 이름을 입력하세요.' name='pj_title_input' id='pj_title_input'></input>
+      <input type='text' placeholder='유튜르 링크를 입력하세요.' name='vd_link_input' id='vd_link_input'></input>
+      <Button link={'/Share'} text={'운동 프로젝트 생성'}></Button>
+      <div className='preview'>
+        <h2 className='title'>동영상 미리보기</h2>
+        <div className='privewVideo'>
+          <div className='videoBox displayNo'>
+            <iframe src="https://www.youtube.com/embed/6ZUIwj3FgUY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
-          <p>미리보기가 없습니다.</p>
-          <p>에러가 발생하였습니다.</p>
+          <p className='previewText '><span>미리보기가 없습니다.</span></p>
+          <p className='previewText displayNo'><span>에러가 발생하였습니다.</span></p>
         </div>
       </div>
     </div>
