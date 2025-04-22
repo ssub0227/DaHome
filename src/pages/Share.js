07e3985a780d8783ec24cd52138159c58bdd29d3
ssub0227 <sb010227@gmail.com>
2025-04-22 18:54:02 +0200

    Fix: Share 페이지 style 수정

--- src/pages/Share.js
+++ src/pages/Share.js
@@ -4,6 +4,13 @@ import Button from 'components/Button';
 import TitleText from 'components/TitleText';
 import SubTitleText from 'components/SubTitleText';
 
+
+let ShareBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
 let BtnList = styled.ul`
   display: flex;
   justify-content:space-between;
@@ -14,11 +21,19 @@ let BtnList = styled.ul`
   }
 `
 
+let Cat = styled.p`
+  font-size: 30vh;
+  margin: 10% auto 0 auto;
+  display: flex;
+  justify-content: center;
+`
+
 const Share = () => {
   return (
-    <div className='share'>
+    <ShareBox>
       <TitleText text={'프로젝트 완성!'} marginBottom={5}></TitleText>
       <SubTitleText text={'친구에게 공유하고 함께 운동해요!'}></SubTitleText>
+      <Cat>¢</Cat>
       <BtnList className='btnList'>
         <li>
           <Button text={'공유하기'}></Button>
@@ -27,7 +42,7 @@ const Share = () => {
           <Button text={'운동하기'} link={'/play'}></Button>
         </li>
       </BtnList>
-    </div>
+    </ShareBox>
   )
 }
 export default Share;
--- src/styles/common.js
+++ src/styles/common.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const CommonCSS = createGlobalStyle`
-
   :root{
     --brown:#472d30;
     --pinkbrown:#723d46;
